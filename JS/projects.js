421021793eca03f0f80031cfd3a84cea2ef5aa03
TheProgrammerofalltime <kma69362@gmail.com>
2025-05-19 20:37:35 -0700

    commit

--- JS/projects.js
+++ JS/projects.js
@@ -1,39 +1,35 @@
 $(document).ready(function(){
     var animationSpeed = 2000
     var pause = animationSpeed + 1000
-    var currentSlide = 1
+    var currentSlide = 0
 
     var $sliderContainer = $('.slides')  
     var $slides = $('.slide')
     var totalSlides = $slides.length
 
-    $sliderContainer.append($slides.first().clone()); // Makes the slideshow a seamless loop
-
-    var interval;
-
     function getSlideWidth(){
         return $('#slider').width(); // Returns slideshow width, no matter what size
     }
 
-    function startSlider(){  
-        clearInterval(interval)
-        interval = setInterval(function(){
-            var width = getSlideWidth(); 
-            $sliderContainer.animate({'margin-left':'-=' + width}, animationSpeed,  
-                function(){  
-                    currentSlide++  
-                    if(currentSlide > totalSlides){  
-                        currentSlide = 1  
-                        $sliderContainer.css('margin-left', 0)  
-                    }  
-                })  
-        },pause)  
+    function goToSlide(index){
+        var width = getSlideWidth();
+        $sliderContainer.stop().animate({'margin-left': -width * index}, 1000)
+        currentSlide = index;
     }
+    
+    $('#next').on('click', function(){
+        var nextSlide = (currentSlide + 1) % totalSlides;
+        goToSlide(nextSlide)
+    })
+
+    $('#prev').on('click', function(){
+        var prevSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+        goToSlide(prevSlide)
+    })
     
     $(window).on('resize', function(){
         $sliderContainer.css('margin-left', 0);
         currentSlide = 1
-        startSlider();
     });
-    startSlider();
+    goToSlide(0)
 });
